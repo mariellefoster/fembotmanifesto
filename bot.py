f30343af4b84949a202b4b302c3aee59411c4d20
Marf Foster <marielle.foster@crowdstrike.com>
2017-05-27 16:25:58 -0500

    bot tweets and manifesto additions and transformation

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
 # @authors Marf and May Waver
 # May 2017
 
@@ -10,20 +12,26 @@ auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 # Get raw text as string.
-with open("manifestos.txt") as f:
+with open("manifestos-lower.txt") as f:
     text = f.read()
 
 # Build the model.
 text_model = markovify.Text(text)
 
-common_sentences = ["By the late twentieth century.",]
+common_sentences = ["By the late twentieth century.", 
+                    "another important thing in life.",
+                    "we take our land.",
+                    "we are survivors who are certain deadweight.",
+                    "in my career. i'd rather make my social interactions profitable to me.",
+                    "<<officer carrie johnson, who had heard debris fall from the outset, expressly to preclude victims from ever exiting a violent situation.",
+                    "this chapter is an optical illusion."]
 
-# # Print three randomly-generated sentences of no more than 140 characters
+# # # Print three randomly-generated sentences of no more than 140 characters
 # for i in range(10):
 #     print(text_model.make_short_sentence(140))
 
 tweet = text_model.make_short_sentence(140)
-while tweet in common_sentences:
+while tweet in common_sentences or tweet is None:
     tweet = text_model.make_short_sentence(140)
 
 api.update_status(tweet)
